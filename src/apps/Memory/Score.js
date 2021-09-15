6ba84be17c189a6b33eb5eb623d39cc4592e2144
AdrianLopez-55 <81708623+AdrianLopez-55@users.noreply.github.com>
2021-09-15 17:58:23 -0400

    Update Score.js

--- src/apps/Memory/Score.js
+++ src/apps/Memory/Score.js
@@ -1,4 +1,3 @@
-
 import React from 'react'
 
 export const Score = ({scoreInit, r, s}) => {
@@ -6,14 +5,10 @@ export const Score = ({scoreInit, r, s}) => {
     scoreInit = 50;
     r = 5;
     s = 12.5;
-    const suma = ({id}) => {
-        if (scoreInit==r){
-            scoreInit=scoreInit+r
-        }
-    }
+
     return (
         <div>
-           {suma}
+     
         </div>
     )
 };
